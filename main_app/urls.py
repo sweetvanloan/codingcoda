d28ee817972ed4ef6a5bccb6af5ef83130463a82
Sweet Van Loan <Sweet@SweetVanLoan.com>
2020-09-24 15:10:23 -0500

    lessons section created, skeleton

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('javascript/', views.javascript, name='javascript'),
+    path('python/', views.python, name='python'),
+    path('ruby/', views.ruby, name='ruby'),
+    path('css/', views.css, name='css'),
 ]
